2eabbb5d995539257e8cacd1bea0b837c5c1144c
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-26 11:16:10 +0530

    Create a class named Food and define variables

---
+++ question-3/Food.java
@@ -0,0 +1,15 @@
+class Food
+{
+	public static void main(String[] args)
+	{
+		String foodName="Paneer Butter Masala";
+		Boolean isVegetarian=true;
+		int calories=400;
+		float price=250.50f;
+		String Cuisine="Indian";
+		System.out.println("Food Name :"+foodName);
+		System.out.println("Cuisine Type :"+Cuisine);
+		System.out.println("Price :Rs"+price);
+		System.out.println("Vegetarian :"+isVegetarian);
+	}
+}
---
+++ question-3/Food2.java
@@ -0,0 +1,12 @@
+class Food2
+{
+	public static void main(String[] args)
+	{
+		String foodName="Paneer Butter Masala";
+		Boolean isVegetarian=true;
+		int calories=400;
+		float price=250.50f;
+		String Cuisine="Indian";
+		System.out.println("Food Name :"+foodName+"\nCuisine Type :"+Cuisine+"\nPrice :Rs"+price+"\nVegetarian :"+isVegetarian);
+	}
+}
