92df531097d4a3cd145a323d01650d2da32d6d27
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-26 11:18:12 +0530

    Create a class named Car and define variables

---
+++ question-4/Car.java
@@ -0,0 +1,16 @@
+class Car
+{
+	public static void main(String[] args)
+	{
+		String carName="Tesla Model S";
+		String fuelType="Electric";
+		String brand="Tesla";
+		short mileage=396;
+		double price=85000.00;
+		System.out.println("Car Name :"+carName);
+		System.out.println("Brand :"+brand);
+		System.out.println("Fuel Type :"+fuelType);
+		System.out.println("Mileage :"+mileage+" miles");
+		System.out.println("Price :Rs"+price);
+	}
+}
---
+++ question-4/Car1.java
@@ -0,0 +1,12 @@
+class Car1
+{
+	public static void main(String[] args)
+	{
+		String carName="Tesla Model S";
+		String fuelType="Electric";
+		String brand="Tesla";
+		short mileage=396;
+		double price=85000.00;
+		System.out.println("Car Name :"+carName+"\nBrand :"+brand+"\nFuel Type :"+fuelType+"\nMileage :"+mileage+" miles"+"\nPrice :Rs"+price);
+	}
+}
