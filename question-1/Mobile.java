45a34f5cdfbed5f33c713f2d17d1259b515feb1b
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-26 11:12:36 +0530

    Create a class named Mobile and create variables

---
+++ question-1/Mobile.java
@@ -0,0 +1,16 @@
+class Mobile
+{
+	public static void main(String[] args)
+	{
+		String mobileName="Vivo Y39";
+		String mobileBrand="Vivo";
+		float screenSize=6.1f;
+		long batteryCapacity=3274l;
+		double price=19236.99;
+		System.out.println(mobileName);
+		System.out.println(mobileBrand);
+		System.out.println(screenSize);
+		System.out.println(batteryCapacity);
+		System.out.println(price);
+	}
+}
